e40a311738428ec2d3725a17884c1e93d089daf8
hoverr-dev <scadygamesru@gmail.com>
2023-05-09 16:35:11 +0300

    Упражнение - Проверка робота

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const bmwX3Price = 100000;
-const budget = 20000;
+/* 
+  Методом prompt получите ответ пользователя
+  на вопрос "Сколько будет 7 + или -15?". Если ответ верен
+  выведите в консоле "Успех", если нет - "Вы робот!",
+  а если он введёт "Я не робот", то тоже "Успех".
+*/
 
-let message = budget > bmwX3Price ? 'BMW' : 'Велосипед';
+const userAnswer = prompt('Сколько будет 7 + или -15?');
 
-console.log(`Я хочу купить ${message}`);
+switch (true) {
+	case userAnswer === 'Я не робот':
+	case Number(userAnswer) === -8:
+	case Number(userAnswer) === 22:
+		console.log('Успех');
+		break;
+	default:
+		console.log('Вы робот!');
+}
