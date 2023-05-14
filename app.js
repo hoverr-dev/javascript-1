68389da0dfe89dad3c6bbafdd2dd55065f043a7b
hoverr-dev <scadygamesru@gmail.com>
2023-05-14 12:30:37 +0300

    55 Упражнение - Расчёт итогового баланса

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
-const arr = [1, 4, 8, 7];
+/* 
+ Есть выгрузка операций пользователя
+ const operations = [1000, -700, 300, -500, 10000];
+ а так же начальный бонус в 100$
+ Необходимо сделать функции расчета:
+ - Итогового баланса
+ - Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
+ - Расчета среднего расхода и среднего дохода
+*/
 
-for (let i = 0; i < arr.length; i++) {
-	console.log(arr[i]);
+const operations = [1000, -700, 300, -500, 10000];
+const startingBalance = 100;
+
+function getBalance(arrayOfOperations, initialBalance) {
+	let balance = initialBalance;
+	for (const el of arrayOfOperations) {
+		balance += el;
+	}
+	return balance;
+}
+console.log(getBalance(operations, startingBalance));
+
+function checkOperations(arrayOfOperations, initialBalance) {
+	let balance = initialBalance;
+	let isTrue = true;
+
+	for (const el of arrayOfOperations) {
+		balance += el;
+		if (balance < 0) {
+			isTrue = false;
+			break;
+		}
+	}
+	return isTrue;
+}
+console.log(checkOperations(operations, startingBalance));
+
+function avarageOperations(arrayOfOperations) {
+	let positiveSum = 0;
+	let positiveCount = 0;
+	let negativeSum = 0;
+	let negativeCount = 0;
+
+	for (const el of arrayOfOperations) {
+		if (el > 0) {
+			positiveCount++;
+			positiveSum += el;
+		}
+
+		if (el < 0) {
+			negativeCount++;
+			negativeSum += el;
+		}
+	}
+	return [positiveSum / positiveCount, negativeSum / negativeCount];
 }
 
-for (let element of arr) {
-	console.log(element);
-}
-
-for (let index in arr) {
-	console.log(arr[index]);
-}
+console.log(avarageOperations(operations));
