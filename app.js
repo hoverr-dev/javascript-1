9ae4947c80110e27e68a0a6ea85e80ec6c8d4730
hoverr-dev <scadygamesru@gmail.com>
2023-05-14 11:59:14 +0300

    53 Цикл while

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-/* 
-for (let i = 1; i < 5; i++) {
-	console.log(`Цикл 1 - ${i}`);
-	for (let j = 1; j < 5; j++) {
-		console.log(`Цикл 2 - ${j}`);
-	}
+/*
+ for (let i = 1; i < 5; i++) {
+	console.log(`Вывод - ${i}`);
+}
+
+let i = 1;
+while (i < 5) {
+	console.log(`Вывод - ${i}`);
+	i++;
+
 } 
 */
 
-const tasks = [
-	[1, 'Задача 1'],
-	[2, 'Задача 2'],
-];
+const arr = [1, 4, 8, 7];
 
-for (let i = 0; i < tasks.length; i++) {
-	for (let j = 0; j < tasks[i].length; j++) {
-		console.log(tasks[i][j]);
+for (let i = 0; i < arr.length; i++) {
+	if (arr[i] > 5) {
+		break;
 	}
+	console.log(arr[i]);
 }
+
+let a = 0;
+while (arr[a] <= 5 && a < arr.length) {
+	console.log(arr[a]);
+	a++;
+}
+
+let j = 0;
+
+do {
+	console.log(j);
+	j++;
+} while (j < 0);
