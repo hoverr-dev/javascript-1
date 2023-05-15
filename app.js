be6eb3a1a16e799ff2fd5fcdc63b9b9c6b233a87
hoverr-dev <scadygamesru@gmail.com>
2023-05-15 10:24:20 +0300

    forEach

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-// function power(pow) {
-// 	return function (num) {
-// 		return num ** pow;
-// 	};
+const score = [5, 10, 0, 15];
+
+// for (const [i, el] of score.entries()) {
+// 	console.log(`Раунд ${i + 1}: ${el}`);
 // }
 
-const power = (pow) => (num) => num ** pow;
+score.forEach((el, i) => {
+	console.log(`Раунд ${i + 1}: ${el}`);
+});
 
-const powerOfTwo = power(2);
-console.log(powerOfTwo(5));
-console.log(powerOfTwo(10));
-
-const powerOfThree = power(3);
-console.log(powerOfThree(5));
-
-console.log(power(5)(4));
+// (5, 0) => { ... }
+// (10, 1) => { ... }
