bc34d5e924d609fad37bbda351d3b3dfaca567ae
hoverr-dev <scadygamesru@gmail.com>
2023-05-15 10:02:59 +0300

    Callback

--- app.js
+++ app.js
@@ -1,58 +1,27 @@
-/* 
- Есть выгрузка операций пользователя
- const operations = [1000, -700, 300, -500, 10000];
- а так же начальный бонус в 100$
- Необходимо сделать функции расчета:
- - Итогового баланса
- - Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
- - Расчета среднего расхода и среднего дохода
-*/
-
-const operations = [1000, -700, 300, -500, 10000];
-const startingBalance = 100;
-
-function getBalance(arrayOfOperations, initialBalance) {
-	let balance = initialBalance;
-	for (const el of arrayOfOperations) {
-		balance += el;
-	}
-	return balance;
-}
-console.log(getBalance(operations, startingBalance));
-
-function checkOperations(arrayOfOperations, initialBalance) {
-	let balance = initialBalance;
-	let isTrue = true;
-
-	for (const el of arrayOfOperations) {
-		balance += el;
-		if (balance < 0) {
-			isTrue = false;
-			break;
-		}
-	}
-	return isTrue;
-}
-console.log(checkOperations(operations, startingBalance));
-
-function avarageOperations(arrayOfOperations) {
-	let positiveSum = 0;
-	let positiveCount = 0;
-	let negativeSum = 0;
-	let negativeCount = 0;
-
-	for (const el of arrayOfOperations) {
-		if (el > 0) {
-			positiveCount++;
-			positiveSum += el;
-		}
-
-		if (el < 0) {
-			negativeCount++;
-			negativeSum += el;
-		}
-	}
-	return [positiveSum / positiveCount, negativeSum / negativeCount];
+function add(a, b) {
+	return a + b;
 }
 
-console.log(avarageOperations(operations));
+function subtruct(a, b) {
+	return a - b;
+}
+
+function power(a, b) {
+	return a ** b;
+}
+
+// Функция высшего порядка
+function calculate(a, b, fn) {
+	console.log(fn.name);
+	const res = fn(a, b);
+	return res;
+}
+
+let res = calculate(3, 5, add);
+console.log(res);
+
+res = calculate(3, 5, subtruct);
+console.log(res);
+
+res = calculate(3, 5, power);
+console.log(res);
