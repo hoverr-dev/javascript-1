e8acae2ac1aea4b7c6b92280366a179613b65710
hoverr-dev <scadygamesru@gmail.com>
2023-05-15 12:18:51 +0300

    Упражнение - среднее значение

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const operations = [100, -20, 7, -30, 50];
+/* 
+Найти среднее значение последовательность
+чисел с помощью reduce
+*/
 
-let balance = 0;
-for (const operation of operations) {
-	balance += operation;
-}
+const arr = [2, 4, 4, 10];
 
-const finalBalance = operations.reduce((acc, operation, i) => {
-	console.log(`Итерация ${i}, acc: ${acc}, operation ${operation}`);
-	return (acc += operation);
-}, 0);
-console.log(finalBalance);
-
-const minElement = operations.reduce((acc, operation) => {
-	if (operation > acc) {
-		return acc;
+const avg = arr.reduce((acc, el, i) => {
+	if (i != arr.length - 1) {
+		return acc + el;
 	} else {
-		return operation;
+		return (acc + el) / arr.length;
 	}
 }, 0);
-console.log(minElement);
+console.log(avg);
