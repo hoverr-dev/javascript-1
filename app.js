9ba7a6f8781735c692fd43799927f953982ce283
hoverr-dev <scadygamesru@gmail.com>
2023-05-15 10:44:39 +0300

    filter

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const transactionInUSD = [10, -7, 50, -10, 100];
-const transactionInRUB = [];
+const operations = [100, -20, 7, -20, 50];
+// const positiveOperations = [];
 
-for (const transaction of transactionInUSD) {
-	transactionInRUB.push(transaction * 60);
-}
+// for (const operation of operations) {
+// 	if (operation > 0) {
+// 		positiveOperations.push(operation);
+// 	}
+// }
 
-// console.log(transactionInUSD);
-// console.log(transactionInRUB);
+// console.log(positiveOperations);
 
-const transactionInRUB2 = transactionInUSD.map((transaction, i) => {
-	console.log(i);
-	return transaction * 60;
+const positiveOperations = operations.filter((operation) => {
+	return operation > 0;
 });
+console.log(positiveOperations);
 
-console.log(transactionInUSD);
-console.log(transactionInRUB2);
+const positiveRUBOperations = operations
+	.filter((operation) => {
+		return operation > 0;
+	})
+	.map((operation) => operation * 60);
+console.log(positiveRUBOperations);
